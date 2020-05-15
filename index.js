df91b6c8213fa087c4728ac2488b56af0d9113cb
Jose Uscanga <jousmo@outlook.com>
2020-05-15 16:48:56 -0500

    Example4 - Scaffolding project base with graphql schema and resolvers, using scalar types

--- index.js
+++ index.js
@@ -3,28 +3,16 @@
 const { buildSchema } = require('graphql')
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
 const app = express()
 const port = process.env.port || 3001
 
 // Definiendo esquema
-const schema = buildSchema(`
-  type Query {
-    "Retorna un saludo al mundo"
-    hello: String
-    "Retorna un gran saludo"
-    saludo: String
-  }
-`)
-
-// Configurar los resolvers
-const resolvers = {
-  hello () {
-    return 'Hola Mundo'
-  },
-  saludo () {
-    return 'Hola a todos'
-  }
-}
+const fileSchema = join(__dirname, 'lib', 'schema.graphql')
+const schema = buildSchema(readFileSync(fileSchema, 'utf-8'))
 
 // Creando una API
 app.use('/api', graphqlHTTP({
