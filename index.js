dbd4468328ad8575d29397b3a2333ea29b58f169
Jose Uscanga <jousmo@outlook.com>
2020-05-17 15:32:28 -0500

    Example20 - Implement Cors and API in Production Config

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+const cors = require('cors')
 const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3001
+const isDev = process.env.NODE_ENV !== 'production'
 
 // Definiendo esquema
 const fileSchema = join(__dirname, 'lib', 'schema.graphql')
 const typeDefs = readFileSync(fileSchema, 'utf-8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+// Definiendo Middlewares
+app.use(cors())
+
 // Creando una API
 app.use('/api', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
-app.listen(port, () => console.log(`Now browse to localhost:${port}/api`))
+app.listen(port, () => {
+  isDev ? console.log(`Now browse to localhost:${port}/api`) : console.log('Server Running...')
+})
